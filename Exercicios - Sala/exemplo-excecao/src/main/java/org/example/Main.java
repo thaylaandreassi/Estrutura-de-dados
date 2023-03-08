079a2d827db19c19a47bcc6ef3612081eeda560a
thaylaandreassi <thaylaandreassi@sptech.school>
2023-03-08 19:12:04 -0300

    ex excecao

--- Exercicios - Sala/exemplo-excecao/src/main/java/org/example/Main.java
+++ Exercicios - Sala/exemplo-excecao/src/main/java/org/example/Main.java
@@ -6,5 +6,22 @@ public class Main {
     public static void main(String[] args) {
         String sNum1 = JOptionPane.showInputDialog("Digite um número: ");
         String sNum2 = JOptionPane.showInputDialog("Digite outro número: ");
+
+        try {
+            int num1 = Integer.parseInt(sNum1);
+            int num2 = Integer.parseInt(sNum2);
+
+            JOptionPane.showMessageDialog(null, num1 / num2);
+        } catch (NumberFormatException e){
+            JOptionPane.showMessageDialog(null, "Digite apenas números\n" + e, "Erro",
+                    JOptionPane.ERROR_MESSAGE);
+        } catch (ArithmeticException e){
+            JOptionPane.showMessageDialog(null, "Divisão por 0\n" + e, "Erro",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+
+        System.exit(0);
+
+
     }
 }
