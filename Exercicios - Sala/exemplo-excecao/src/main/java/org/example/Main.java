421b4b7abba5960ed949f87efce41a8d1ab35dbd
thaylaandreassi <thaylaandreassi@sptech.school>
2023-03-08 18:16:28 -0300

    ex excecao

---
+++ Exercicios - Sala/exemplo-excecao/src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        String sNum1 = JOptionPane.showInputDialog("Digite um número: ");
+        String sNum2 = JOptionPane.showInputDialog("Digite outro número: ");
+    }
+}
